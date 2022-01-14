1d63648fa845db29f158b5c3cb48103f1db09333
sper0054 <teresa.sperl@gmail.com>
2022-01-14 15:08:31 -0600

    NY times API

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -30,37 +30,15 @@ var getBooksList = function(books) {
 
 
 
-//youtube api start
-//source: https://developers.google.com/youtube/iframe_api_reference?hl=en_US
-// This code loads the IFrame Player API code asynchronously.
-var tag = document.createElement('script');
 
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-// This function creates an <iframe> (and YouTube player)
-//    after the API code downloads.
-var player;
-function onYouTubeIframeAPIReady() {
-    player = new YT.Player('player', {
-        height: '390',
-        width: '640',
-        videoId: 'M7lc1UVf-VE',
-        playerVars: {
-            'playsinline': 1
-        },
-        events: {
-        'onReady': onPlayerReady,
-        'onStateChange': onPlayerStateChange
-        }
-        });
-}
 
-// The API will call this function when the video player is ready.
-function onPlayerReady(event) {
-event.target.playVideo();
-}
+
+
+
+
+
+      
 
 // user input array
 var search = {};
