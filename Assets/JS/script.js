def87730504ad9c6164a5cd39a28798684a4a5c5
sper0054 <teresa.sperl@gmail.com>
2022-01-15 09:58:48 -0600

    working NYT API

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,45 +1,25 @@
+// defining variables
+var bookList = document.querySelector('ul');
+var fetchButton = document.getElementById('bestsellerbtn');
 
-//variables to store api keys
+// API Key
 var bookAPI= "D9OwWTZWlrbbFIzrqzKyzY9zxhC4MVua"
-var tubeAPI= "AIzaSyBSpft2lGGm4UmvupTZwpBsVuamB3inYA0"
 
 
-//books api start
-//source: https://developer.nytimes.com/docs/books-product/1/routes/lists/overview.json/get
-var getBooksList = function(books) {
-    //overview endpoint will list Top 5 books for all Best Sellers lists
-    var apiUrl = "https://api.nytimes.com/svc/books/v3/lists/overview.json";
-    // make a get request to url
-    fetch(apiUrl)
-      .then(function(response) {
-        // request was successful
-        if (response.ok) {
-          console.log(response);
-          response.json().then(function(data) {
-            console.log(data);
-            displayBooks(data, books);
-          });
-        } else {
-          alert("Error: " + response.statusText);
-        }
-      })
-      .catch(function(error) {
-        alert("Unable to connect to NY Times");
-      });
+function getApi() {
+  var requestUrl = "https://api.nytimes.com/svc/books/v3/lists/overview.json?api-key=" + bookAPI;
+  fetch(requestUrl)
+  .then(function(response) {
+    console.log (response)
+    return response.json();
+  })
+  .then(function(data) {
+    console.log (data)
+    for (var i = 0; i < 5; i++) {
+     var listItem = document.createElement('li');
+      listItem.textContent = data.results.lists[0].title;
+    }
+  });
 };
 
-
-
-
-
-
-
-
-
-
-
-      
-
-// user input array
-var search = {};
-
+fetchButton.addEventListener('click', getApi);
